c83b5b33a1dfa0e1ebcd6999e85d3aca46ef7a6a
Psj109079 <ckckckemfdjdhk@gmail.com>
2023-06-21 16:25:56 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import img from "./sexyhomin.gif";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <br/><br/><br/><br/>
+      <img alt="" src={img} className="App-logo"/>
+      <br/><br/>
+      <h2>Docker 배포 성공 기원~</h2>
+    </div>
+  );
+}
+
+export default App;
