c44e1c858236fd973d7228cf86785506394c9ae5
Psj109079 <ckckckemfdjdhk@gmail.com>
2023-06-22 17:31:21 +0900

    a

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,10 +1,12 @@
 import img from "./sexyhomin.gif";
+import bomb from "./image/bombberjoo.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <img alt="" src={img} style={{marginTop: "100px"}} className="App-logo"/>
+        <img src={bomb} className="joo"/>
       <br/><br/>
       <h2>Docker 배포 성공?!?!?!?!?</h2>
     </div>
