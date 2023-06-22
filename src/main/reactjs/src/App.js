5ce2a30ebce6b15d29e5dea9e6e3e661b7ea6c79
Psj109079 <ckckckemfdjdhk@gmail.com>
2023-06-22 16:23:20 +0900

    success

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <img alt="" src={img} style={{marginTop: "100px"}} className="App-logo"/>
       <br/><br/>
-      <h2>Docker 배포 성공 기원~</h2>
+      <h2>Docker 배포 성공?!?!?!?!?</h2>
     </div>
   );
 }
