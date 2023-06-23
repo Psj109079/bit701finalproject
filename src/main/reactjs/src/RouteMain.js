8516efca3600458107258697463ebe705a31d924
Psj109079 <ckckckemfdjdhk@gmail.com>
2023-06-23 15:33:56 +0900

    suc

--- src/main/reactjs/src/RouteMain.js
+++ src/main/reactjs/src/RouteMain.js
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import {Home, Menu} from "./components";
+import {Route, Routes} from "react-router-dom";
+import {LoginForm, MemberForm, MemberList} from "./member";
+import {BoardForm, BoardList} from "./board";
+import img from "./image/chim0unbg.gif";
+import "./App.css"
 function RouteMain(props) {
     return (
-        <div>
+        <div className="App">
+            <Menu/>
+            <br style={{clear: "both"}}/><br/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<LoginForm/>}/>
+                <Route path="/member">
+                    <Route path="form" element={<MemberForm/>}/>
+                    <Route path="list" element={<MemberList/>}/>
+                </Route>
+                <Route path="/board">
+                    <Route path="form" element={<BoardForm/>}/>
+                    <Route path="list" element={<BoardList/>}/>
+                    <Route path="list:currentPage" element={<BoardList/>}/>
+                </Route>
 
+                <Route path="*" element={
+                   <div>
+                       <img alt="" src={img} className="zero"/>
+                       <h1>잘못된 URL 주소입니다</h1>
+                   </div>
+                }/>
+            </Routes>
         </div>
     );
 }
